62829992f4f36797a012530748025e56500bb0c0
tomaszmjurek <tomaszmjurek@gmail.com>
2020-01-16 15:09:35 +0100

    Add test client

--- app/src/main/java/com/example/shutup/MainActivity.kt
+++ app/src/main/java/com/example/shutup/MainActivity.kt
@@ -8,9 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //startConnection()
     }
 
     private fun startConnection() {
-
+        val testCl1 = TestClient()
+        var testSer = MyServer()
+        testSer.run()
+        testCl1.connect()
     }
 }
